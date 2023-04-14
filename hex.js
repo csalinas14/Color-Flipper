0730bc2a81f2452a825582ada477812985a4b3d2
Carlos Salinas <salinas.carlos14@gmail.com>
2023-04-14 14:41:18 -0400

    Completing hex.js

--- hex.js
+++ hex.js
@@ -3,5 +3,15 @@ const btn = document.getElementById('btn');
 const color = document.querySelector(".color");
 
 btn.addEventListener('click', function(){
+    let hexColor = '#';
+    for(let i=0; i<6; i++){
+        hexColor += hex[getRandomNumber()];
+    }
+    console.log(hexColor);
+    color.textContext = hexColor;
+    document.body.style.backgroundColor = hexColor;
+});
 
-})
+function getRandomNumber(){
+    return Math.floor(Math.random() * hex.length);
+}
